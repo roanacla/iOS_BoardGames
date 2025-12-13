1cac562620f7d39519c3bd81161f4d129e7277f6
Roger Navarro <roanacla@me.com>
2025-12-12 18:01:06 -0800

    Initial Commit

---
+++ iOS_BoardGames/iOS_BoardGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_BoardGamesApp.swift
+//  iOS_BoardGames
+//
+//  Created by Roger Navarro Claros on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_BoardGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS_BoardGamesUITests/iOS_BoardGamesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_BoardGamesUITestsLaunchTests.swift
+//  iOS_BoardGamesUITests
+//
+//  Created by Roger Navarro Claros on 12/12/25.
+//
+
+import XCTest
+
+final class iOS_BoardGamesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
