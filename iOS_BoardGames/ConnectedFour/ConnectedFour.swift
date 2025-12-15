7f82c1dd04f586fa765072da609d6674b08478c3
Roger Navarro <roanacla@me.com>
2025-12-15 14:12:46 -0800

    assignment_3

--- iOS_BoardGames/ConnectedFour/ConnectedFour.swift
+++ iOS_BoardGames/ConnectedFour/ConnectedFour.swift
@@ -26,4 +26,27 @@ struct ConnectedFour {
         board[current, column] = piece
         return (current, column)
     }
+    
+    public func checkWinner(forRow row: Int, col: Int, piece: CFSlot) throws -> Bool {
+        guard row >= 0, row < board.height, col >= 0, col < board.width else {
+            throw GameError.invalidPosition
+        }
+        guard board[row, col] == piece else {
+            return false
+        }
+        let isWinner = countMatches(startingFrom: row, col: col, dRow: -1, dCol: 0, piece: piece) + countMatches(startingFrom: row, col: col, dRow: 1, dCol: 0, piece: piece) + 1 >= 4 ||
+        countMatches(startingFrom: row, col: col, dRow: 0, dCol: -1, piece: piece) + countMatches(startingFrom: row, col: col, dRow: 0, dCol: 1, piece: piece) + 1 >= 4 ||
+        countMatches(startingFrom: row, col: col, dRow: -1, dCol: 1, piece: piece) + countMatches(startingFrom: row, col: col, dRow: 1, dCol: -1, piece: piece) + 1 >= 4 ||
+        countMatches(startingFrom: row, col: col, dRow: -1, dCol: -1, piece: piece) + countMatches(startingFrom: row, col: col, dRow: 1, dCol: 1, piece: piece) + 1 >= 4
+        return isWinner
+    }
+    
+    private func countMatches(startingFrom row: Int, col: Int, dRow: Int, dCol: Int, piece: CFSlot) -> Int {
+        let nextRow = row + dRow
+        let nextColumn = col + dCol
+        guard nextRow >= 0, nextRow < board.height, nextColumn >= 0, nextColumn < board.width, board[nextRow, nextColumn] == piece else {
+            return 0
+        }
+        return 1 + countMatches(startingFrom: nextRow, col: nextColumn, dRow: dRow, dCol: dCol, piece: piece)
+    }
 }
--- iOS_BoardGames/ConnectedFour/GameError.swift
+++ iOS_BoardGames/ConnectedFour/GameError.swift
@@ -1,4 +1,5 @@
 enum GameError: Error {
     case columnFull
     case invalidColumn
+    case invalidPosition
 }
